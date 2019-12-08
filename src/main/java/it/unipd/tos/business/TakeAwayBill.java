1ad72f918b2cd1729a45a4c203de6d744e85fca3
outshudi <djcongojac@gmail.com>
2019-12-09 00:50:36 +0100

    close #7

---
+++ src/main/java/it/unipd/tos/business/TakeAwayBill.java
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////////////////////////
+// [Olivier] [Utshudi] [1143556]
+////////////////////////////////////////////////////////////////////
+package it.unipd.tos.business;
+
+import java.util.List;
+
+import it.unipd.tos.business.exception.TakeAwayBillException;
+import it.unipd.tos.model.MenuItem;
+
+public interface TakeAwayBill {
+    double getOrderPrice(List<MenuItem> itemsOrdered) throws TakeAwayBillException;
+}
---
+++ src/main/java/it/unipd/tos/business/exception/TakeAwayBillException.java
@@ -0,0 +1,10 @@
+////////////////////////////////////////////////////////////////////
+// [Olivier] [Utshudi] [1143556]
+////////////////////////////////////////////////////////////////////
+package it.unipd.tos.business.exception;
+
+public class TakeAwayBillException extends Exception {
+    public TakeAwayBillException(String message) {
+        super(message);
+    }
+}
